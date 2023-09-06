8b6dc4f7ed5657e04f9dab8f81261dbfb4ccfb14
Petter Olsson <petter@dominodatalab.com>
2023-09-06 00:38:22 -0700

    Updated unblind to export to CSV
    Pushed from Domino: https://pharma.domino-eval.com/workspace/CDISC01_Team/CDISC01_RE_INTERIM?executionId=64f703d1b9f2c63b043b238a

--- qc/adam/unblind.R
+++ qc/adam/unblind.R
@@ -1,5 +1,6 @@
-# Load the haven package
+# Install and load the necessary packages
 library(haven)
+library(readr)
 
 # Set the path to the directory where the data resides
 path <- "/mnt/data/ADAM"
@@ -10,6 +11,14 @@ files <- list.files(path, pattern = "\\.sas7bdat$", full.names = TRUE)
 # Read each file and store them in a list
 data_list <- lapply(files, read_sas)
 
+# Save the data_list to an R data file in the same directory
+save(data_list, file = file.path(path, "combined_data.RData"))
+
+cat("Data saved to", file.path(path, "combined_data.RData"), "\n")
+
+# Load the .RData file
+load(file.path(path, "combined_data.RData"))
+
 # Specify the output directory
 output_dir <- "/mnt/artifacts/results"
 
@@ -18,7 +27,9 @@ if (!dir.exists(output_dir)) {
   dir.create(output_dir, recursive = TRUE)
 }
 
-# Save the data_list to an R data file
-save(data_list, file = file.path(output_dir, "combined_data.RData"))
+# Export each dataset from data_list as a CSV file
+for(i in seq_along(data_list)) {
+  write_csv(data_list[[i]], file = file.path(output_dir, paste0("data_", i, ".csv")))
+}
 
-cat("Data saved to", file.path(output_dir, "combined_data.RData"))
+cat("Data exported to", output_dir)
